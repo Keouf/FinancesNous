86ad5ac50bb25f6553ac071366d91bd62919a6f4
Vidjay Seganti <keouf@hotmail.fr>
2016-01-21 13:19:56 +0100

    Icon application. Menu accueil.

--- app/src/main/java/com/example/pcportablevidjay/financesnous/Accueil.java
+++ app/src/main/java/com/example/pcportablevidjay/financesnous/Accueil.java
@@ -82,15 +82,13 @@ public class Accueil extends AppCompatActivity
 
         if (id == R.id.nav_depense) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_garantie) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_noteDeFrais) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_about) {
 
         } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
