8179d6acb57a5f917f73bf5939930cd1acd1b6ca
peter <peter.berghuis_13@hotmail.com>
2016-02-04 17:01:49 +0100

    peter.

--- app/src/main/java/classes/Depense.java
+++ app/src/main/java/classes/Depense.java
@@ -3,7 +3,7 @@ package classes;
 
 import java.util.Date;
 
-public class Depense {
+public class Depense{
 
     private Integer idDepense;
     private Date dateDepense;
@@ -16,42 +16,55 @@ public class Depense {
     public Integer getIdDepense() {
         return idDepense;
     }
+
     public void setIdDepense(Integer idDepense) {
         this.idDepense = idDepense;
     }
+
     public Date getDateDepense() {
         return dateDepense;
     }
+
     public void setDateDepense(Date dateDepense) {
         this.dateDepense = dateDepense;
     }
+
     public double getMontant() {
         return montant;
     }
+
     public void setMontant(double montant) {
         this.montant = montant;
     }
+
     public String getPieceJoint() {
         return pieceJoint;
     }
+
     public void setPieceJoint(String pieceJoint) {
         this.pieceJoint = pieceJoint;
     }
+
     public Magasin getMagasin() {
         return magasin;
     }
+
     public void setMagasin(Magasin magasin) {
         this.magasin = magasin;
     }
+
     public String getDomaine() {
         return domaine;
     }
+
     public void setDomaine(String domaine) {
         this.domaine = domaine;
     }
+
     public Utilisateur getUtilisatuer() {
         return utilisatuer;
     }
+
     public void setUtilisatuer(Utilisateur utilisatuer) {
         this.utilisatuer = utilisatuer;
     }
@@ -65,4 +78,27 @@ public class Depense {
         this.magasin = magasin;
         this.pieceJoint = pieceJoint;
     }
+
+//    @Override
+//    public int describeContents() {
+//        return 0;
+//    }
+//
+//    @Override
+//    public void writeToParcel(Parcel dest, int flags) {
+//        dest.writeValue(this);
+//    }
+//
+//    public static final Parcelable.Creator<Depense> CREATOR = new Parcelable.Creator<Depense>() {
+//        public Depense createFromParcel(Parcel in) {
+//            //return new Depense(in);
+//            return null;
+//        }
+//    }
+//
+//    @Override
+//    public Depense[] newArray(int size) {
+//        return new Depense[size];
+//    }
 }
+
--- app/src/main/java/classes/DepenseAdapter.java
+++ app/src/main/java/classes/DepenseAdapter.java
@@ -2,12 +2,14 @@ package classes;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.example.pcportablevidjay.financesnous.DepenseDetail_Activity;
 import com.example.pcportablevidjay.financesnous.R;
 
 import java.text.SimpleDateFormat;
@@ -22,11 +24,20 @@ public class DepenseAdapter extends ArrayAdapter<Depense> {
     public View getView(int position, View convertView, ViewGroup parent) {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd");
 
-        Depense depense = getItem(position);
+        final Depense depense = getItem(position);
 
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.depense_item, parent, false);
         }
+
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), DepenseDetail_Activity.class);
+                //intent.putExtra("Depense", depense);
+            }
+        });
+
         TextView tvNom = (TextView) convertView.findViewById(R.id.tvNom);
         TextView tvDate = (TextView) convertView.findViewById(R.id.tvDate);
         TextView tvMontant = (TextView) convertView.findViewById(R.id.tvMontant);
