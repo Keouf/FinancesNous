2a8adfc423d9d7ac9884ce5f1fc6b5a6e96b1825
Antoine <antoine.goncalves1995@orange.fr>
2016-02-12 10:03:14 +0100

    Les tests unitaires

--- app/src/test/java/com/example/pcportablevidjay/financesnous/ExampleUnitTest.java
+++ app/src/test/java/com/example/pcportablevidjay/financesnous/ExampleUnitTest.java
@@ -8,6 +8,19 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
+   /* UN EXEMPLE CONCRÊT */
+//    private MainActivity mCalculator;
+//
+//    @Before
+//    public void setUp() {
+//        mCalculator = new MainActivity();
+//    }
+//
+//    @Test
+//    public void addTwoNumbers() {
+//        double resultAdd = mCalculator.add(1d, 1d);
+//        assertThat(resultAdd, is(equalTo(2d)));
+//    }
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
