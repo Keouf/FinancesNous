768f4fb1fadac5f8892e3addc0537c5af3d21ba3
Dj00lls <j.elluin@gmail.com>
2016-03-31 12:11:52 +0200

    Commit de changement pour les test unitaires

--- app/src/main/java/com/example/pcportablevidjay/financesnous/Fragment_Accueil.java
+++ app/src/main/java/com/example/pcportablevidjay/financesnous/Fragment_Accueil.java
@@ -11,7 +11,6 @@ public class Fragment_Accueil extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_accueil, container, false);
-        return view;
+        return inflater.inflate(R.layout.fragment_accueil, container, false);
     }
 }
