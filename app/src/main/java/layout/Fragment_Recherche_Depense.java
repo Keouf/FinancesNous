655c137dc0a78a101956b3a8cae2150936fed277
peter <peter.berghuis_13@hotmail.com>
2016-04-01 15:05:09 +0200

    fragment_recherche_depense affiche message si aucune depense trouvé

--- app/src/main/java/layout/Fragment_Recherche_Depense.java
+++ app/src/main/java/layout/Fragment_Recherche_Depense.java
@@ -62,7 +62,7 @@ public class Fragment_Recherche_Depense extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        Log.e("json", "kill me : " + StorageHelper.getUtilisateur(getActivity().getBaseContext()).getAllDepensesInString());
+        Log.e("json", "all 10 depenses : " + StorageHelper.getUtilisateur(getActivity().getBaseContext()).getAllDepensesInString());
         if (StorageHelper.getUtilisateur(getActivity().getBaseContext()).getMesDepenses().size() > 0)
         {
             adapater.clear();
