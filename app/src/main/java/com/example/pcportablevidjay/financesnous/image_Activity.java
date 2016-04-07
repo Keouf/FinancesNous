e51267c5723dccc3cd2f5fd9b2c77b7c69431c2c
BerghuisPeter <peter.berghuis_13@hotmail.com>
2016-04-07 22:38:39 +0200

    +++

---
+++ app/src/main/java/com/example/pcportablevidjay/financesnous/image_Activity.java
@@ -0,0 +1,69 @@
+package com.example.pcportablevidjay.financesnous;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+
+public class image_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_);
+
+        Intent i = getIntent();
+        String url = (String) i.getSerializableExtra("url");
+
+
+        new DownloadImageTask((ImageView) findViewById(R.id.activity_image_imageView))
+                .execute(url);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+                in.close();
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            Bitmap scaled = Bitmap.createScaledBitmap(result, result.getWidth() / 3, result.getHeight() / 3, false);
+            bmImage.setImageBitmap(scaled);
+        }
+    }
+
+}
